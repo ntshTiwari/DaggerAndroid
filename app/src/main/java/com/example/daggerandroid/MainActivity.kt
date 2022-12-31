a11243b6d786988133c3eff919033e02fcc44df0
Nitesh Kumar <niteshk@geekyants.com>
2023-01-01 00:07:05 +0530

    feat: Added code that uses manual dependency injection

--- app/src/main/java/com/example/daggerandroid/MainActivity.kt
+++ app/src/main/java/com/example/daggerandroid/MainActivity.kt
@@ -3,9 +3,16 @@ package com.example.daggerandroid
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity(R.layout.activity_main) {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main)
+
+        val userRepository = UserRepository();
+        val emailService = EmailService();
+
+        /// as we are manually creating objects and passing it to the class, so it follows manual dependency injection
+        val userRegistrationService = UserRegistrationService(userRepository, emailService)
+        userRegistrationService.resisterUser("nitesh@mail.com", "password")
     }
 }
--- app/src/main/java/com/example/daggerandroid/UserRegistrationService.kt
+++ app/src/main/java/com/example/daggerandroid/UserRegistrationService.kt
@@ -1,12 +1,11 @@
 package com.example.daggerandroid
 
-class UserRegistrationService {
-    /// as these variables are closely binded with the class,
-    /// so it is hard to test
-    /// it does not follow any dependency injection pattern
-    private val userRepository = UserRepository();
-    private val emailService = EmailService();
-
+/// now the parameters can be passed to this class
+/// so, now they are not closely binded
+class UserRegistrationService(
+    private val userRepository: UserRepository,
+    private val emailService: EmailService,
+) {
     fun resisterUser(email: String, password: String){
         userRepository.saveUser(email, password);
         emailService.send(email, "no-resply@google.com", "body")
