9650627ddcdb44f534730ec4e781be76c692e0fa
Nitesh Kumar <niteshk@geekyants.com>
2023-01-01 23:03:32 +0530

    feat: Changed for loosely coupled architecture

--- app/src/main/java/com/example/daggerandroid/EmailService.kt
+++ app/src/main/java/com/example/daggerandroid/EmailService.kt
@@ -4,10 +4,20 @@ import android.content.ContentValues.TAG
 import android.util.Log
 import javax.inject.Inject
 
+interface NotificationService {
+    fun send(to: String, from: String, body: String?)
+}
+
 /// here we use the [@Inject] annotation, which mean when dagger has to inject this object it can
 /// create it using the constructor of the EmailService class
-class EmailService @Inject constructor() {
-    fun send(to: String, from: String, body: String?){
+class EmailService @Inject constructor() : NotificationService {
+    override fun send(to: String, from: String, body: String?){
         Log.e(TAG, "Email sent from: $from to: $to");
+    }
+}
+
+class SmsService @Inject constructor() : NotificationService {
+    override fun send(to: String, from: String, body: String?){
+        Log.e(TAG, "SMS sent from: $from to: $to");
     }
 }
--- app/src/main/java/com/example/daggerandroid/UserRepository.kt
+++ app/src/main/java/com/example/daggerandroid/UserRepository.kt
@@ -4,10 +4,22 @@ import android.content.ContentValues.TAG
 import android.util.Log
 import javax.inject.Inject
 
+/// using interface we can implement a loosely coupled architecture
+/// with which we can pass different classes based on the requirements
+interface UserRepository{
+    fun saveUser(email: String, password: String)
+}
+
 /// here we use the [@Inject] annotation, which mean when dagger has to inject this object it can
 /// create it using the constructor of the UserRepository class
-class UserRepository @Inject constructor() {
-    fun saveUser(email: String, password: String){
-        Log.e(TAG, "User $email Saved in DB");
+class SQLRepository @Inject constructor(): UserRepository {
+    override fun saveUser(email: String, password: String){
+        Log.e(TAG, "User $email Saved in SQL DB");
+    }
+}
+
+class FirebaseRepository @Inject constructor(): UserRepository {
+    override fun saveUser(email: String, password: String){
+        Log.e(TAG, "User $email Saved in Firebase DB");
     }
 }
