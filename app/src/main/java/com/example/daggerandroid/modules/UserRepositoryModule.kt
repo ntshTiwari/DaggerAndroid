4ec7e5a6f27374a9e061a44e70f8eb938001a37b
Nitesh Kumar <niteshk@geekyants.com>
2023-01-05 14:04:23 +0530

    feat: Changes UserRepositoryModule @Provides method to a @Bind method

--- app/src/main/java/com/example/daggerandroid/modules/UserRepositoryModule.kt
+++ app/src/main/java/com/example/daggerandroid/modules/UserRepositoryModule.kt
@@ -2,17 +2,22 @@ package com.example.daggerandroid.modules
 
 import com.example.daggerandroid.FirebaseRepository
 import com.example.daggerandroid.UserRepository
+import dagger.Binds
 import dagger.Module
 import dagger.Provides
 
 @Module
-class UserRepositoryModule {
+abstract class UserRepositoryModule {
 
     /// @Provides tells Dagger that, whenever it needs an element of UserRepository,
     /// it can use the function getFirebaseRepository() to create it
-    @Provides
-    fun getFirebaseRepository(): UserRepository {
-        /// we can put our own logic here to create the object, it can be any logic
-        return FirebaseRepository();
-    }
+//    @Provides
+//    fun getFirebaseRepository(firebaseRepository: FirebaseRepository): UserRepository {
+//        /// we can put our own logic here to create the object, it can be any logic
+//        return FirebaseRepository();
+//    }
+
+    /// the above `@Provides` method can be changed to `@Binds` method like this
+    @Binds
+    abstract fun getFirebaseRepository(firebaseRepository: FirebaseRepository): UserRepository;
 }
